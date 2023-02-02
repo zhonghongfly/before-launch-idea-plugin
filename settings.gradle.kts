ec87824ae482aff0189724b5d2c2f83354b35a76
GitHub Action <action@github.com>
2023-02-02 10:52:24 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "before-launch-idea-plugin"
--- src/main/kotlin/com/github/zhonghongfly/beforelaunchideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zhonghongfly/beforelaunchideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhonghongfly.beforelaunchideaplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhonghongfly.beforelaunchideaplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
